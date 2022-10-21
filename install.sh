930ef4a29e0496464d9b1e5bfe58bb43a391114c
Mathias Peters <mathias.peters6@gmail.com>
2022-10-21 21:33:49 +0200

    Added intall and setup scripts for ubuntu

---
+++ install.sh
@@ -0,0 +1,12 @@
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+chmod +x /usr/local/bin/oh-my-posh
+
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.omp.*
+rm ~/.poshthemes/themes.zip
+
+cd fonts
+apt-get install fontconfig
+./install.sh
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 pwd=`pwd` 
 
-posh1='eval "$(oh-my-posh-wsl --init --shell bash --config '
+posh1='eval "$(oh-my-posh init bash --config '
 posh2='/appearance/profile.omp.json)"'
 ohMyPosh=$posh1$pwd$posh2
 echo $ohMyPosh >> ~/.profile
@@ -9,3 +9,5 @@ git1='. "'
 git2='/git-magic/bash.sh"'
 gitMagic=$git1$pwd$git2
 echo $gitMagic >> ~/.profile
+
+. ~/.profile
